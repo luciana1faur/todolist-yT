61fa5e0ddeba42741ebe5a1e1d9e50b245b3bd5b
Faur Luciana <luciana_faur@yahoo.com>
2021-09-27 10:29:28 +0300

    https://github.com/luciana1faur/todolist-yT.git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+//after this tutorial: https://www.youtube.com/watch?v=pCA4qpQDZD8 salvat in calc c:/apliReact/todolist
+import React, {useState, useEffect} from "react";
 import './App.css';
+//importing components
+import Form from "./componenets/Form";
+import TodoList from "./componenets/TodoList";
 
 function App() {
+
+  //State stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+    //Use effect
+    useEffect(() => {
+      filterHandler();
+    }, [todos, status]);
+
+  //Functions 
+  const filterHandler = () => {
+    switch(status) {
+      case "completed": 
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+      break;
+      case "uncompleted": 
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+      break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Luciana's Todo List</h1>
       </header>
+
+      <Form 
+        inputText = {inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      
+    <TodoList 
+      filteredTodos={filteredTodos}
+      setTodos={setTodos} 
+      todos={todos}
+    />
     </div>
   );
 }
